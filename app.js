7837c6ddc7208c5c998a810950389e940e90f7db
unknown <raquelcorporales@gmail.com>
2022-05-28 23:05:47 +0200

    cors3

--- app.js
+++ app.js
@@ -1,47 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-require('dotenv/config');
-const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv/config");
+const mongoose = require("mongoose");
 app.use(bodyParser.json());
 
-
 //IMPORTAR RUTAS
-const pruebaRuta = require('./routes/prueba')
+const pruebaRuta = require("./routes/prueba");
 
 //BASE DE DATOS
-mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true,useUnifiedTopology: true}).then(console.log('conectado a la base de datos')).catch(err => console.error(err));
+mongoose
+  .connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("conectado a la base de datos"))
+  .catch((err) => console.error(err));
 
 //MIDDLEWARES
- app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:',app.get('port'));
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 app.use(cors());
-app.use('/', pruebaRuta);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.headers.origin); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-
+app.use("/", pruebaRuta);
 
 //SETTINGS
-app.set('port', process.env.PORT || 3000);
-
+app.set("port", process.env.PORT || 3000);
 
 //SERVER
-app.listen(app.get('port'), ()=>{
-    console.log('server on port', app.get('port'));
+app.listen(app.get("port"), () => {
+  console.log("server on port", app.get("port"));
 });
