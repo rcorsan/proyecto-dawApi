7486e4a5addec199b2c5b5b9cf9dd6cf19e6262a
unknown <raquelcorporales@gmail.com>
2022-05-28 23:55:24 +0200

    cors3

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const cors = require('cors');
+//const cors = require('cors');
 require('dotenv/config');
 const mongoose = require('mongoose');
 app.use(bodyParser.json());
-app.use(cors());
+app.use(function(req,res,next){
+    res.header('Access-Control-Allows-Origin', '*');
+    res.header('Access-Control-Allows-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allows-Headers', 'Content-Type');
+});
 
 
 //IMPORTAR RUTAS
