fb873a5419f30e3fa3bae1371054b0e0cecfe262
unknown <raquelcorporales@gmail.com>
2022-05-29 16:55:12 +0200

    gets

--- routes/prueba.js
+++ routes/prueba.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const User = require('../models/user');
 const consumables = require('../models/consumables');
-const enemiess = require('../models/enemies');
+const enemies = require('../models/enemies');
 const skills = require('../models/skills');
 const equpment = require('../models/equipments');
 
@@ -14,6 +14,21 @@ router.get('/consumables', async (req,res)=>{
     //res.json(JSON.stringify(await consumables.find()));
     const fcon= await consumables.find();
     res.json(fcon);
+});
+router.get('/enemies', async (req,res)=>{
+    //res.json(JSON.stringify(await consumables.find()));
+    const fen= await enemies.find();
+    res.json(fen);
+});
+router.get('/skills', async (req,res)=>{
+    //res.json(JSON.stringify(await consumables.find()));
+    const fsk= await skills.find();
+    res.json(fsk);
+});
+router.get('/equipment', async (req,res)=>{
+    //res.json(JSON.stringify(await consumables.find()));
+    const fequi= await equpment.find();
+    res.json(fequi);
 });
 
 router.post('/', (req,res) => {
